babf545d552a8255c760e5115dc5a549bd517726
takuma matsunaga <matsu@takumanoMacBook-Pro.local>
2022-09-11 14:46:33 +0900

    01

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tweet;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+use Illuminate\View\Factory;
 
 class IndexController extends Controller
 {
@@ -17,8 +20,24 @@ class IndexController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, Factory $factory)
     {
-        return 'single Action';
+
+        $tweets = Tweet::all();
+        // Laravel独自のヘルパー関数。dumn,dieの頭文字で、その場で処理を中断して変数の内容などを出力する
+        // dd($tweets);
+
+        // viewヘルパー関数でのViewの呼び方
+        return view('tweet.index', ['tweets' => $tweets]);
+
+        // テンプレートへ別の変数の渡し方
+        // with関数はチェーンメソッドで何度も呼び出し可能
+        // return view('tweet.index')->with('name', 'laravel')->with('version', '8');
+
+        // Facadeでの呼び方
+        // return View::make('tweet.index', ['name' => 'laravel']);
+
+        // Factoryをインジェクションしての呼び方
+        // return $factory->make('tweet.index', ['name' => 'laravel']);
     }
 }
---
+++ app/Models/Tweet.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tweet extends Model
+{
+    use HasFactory;
+
+    // テーブル名とクラス名の紐付けが必要な場合
+    // protected $stable = 'tweet';
+
+    // 主キーの名前がidではなくtweet_idなど別名であった場合
+    // protected $primaryKey = 'tweet_id';
+
+    // 主キーが増分整数値(auto increment)ではなくUUIDなどの場合
+    // 主キーが増分整数ではないことの宣言
+    // public $incrementing = false;
+    // 主キーが整数でない場合
+    // protected $keyType = 'string';
+
+    // created_at,updated_atが不要な場合
+    // public $timestamps = false;
+    // 対応するカラム名を指定
+    // const CREATED_AT = 'creation_date';
+    // const UPDATED_AT = 'updated_date';
+}
