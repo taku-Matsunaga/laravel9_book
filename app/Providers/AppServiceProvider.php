16dd0bdefb7050eca8f4753b30d550a9dc6e0b4e
taku-Matsunaga <skyline199306@yahoo.co.jp>
2022-09-12 19:34:33 +0900

    complete

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\Modules\ImageUpload\CloudinaryImageManager;
+use App\Modules\ImageUpload\ImageManagerInterface;
+use App\Modules\ImageUpload\LocalImageManager;
 use Illuminate\Support\ServiceProvider;
+use Cloudinary\Cloudinary;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +17,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(Cloudinary::class, function () {
+            return new Cloudinary([
+                'cloud' => [
+                    'cloud_name' => config('cloudinary.cloud_name'),
+                    'api_key'    => config('cloudinary.api_key'),
+                    'api_secret' => config('cloudinary.api_secret'),
+                ],
+            ]);
+        });
+        if ($this->app->environment('production')) {
+            $this->app->bind(ImageManagerInterface::class, CloudinaryImageManager::class);
+        } else {
+            $this->app->bind(ImageManagerInterface::class, LocalImageManager::class);
+        }
     }
 
     /**
--- app/Services/TweetService.php
+++ app/Services/TweetService.php
@@ -4,12 +4,17 @@ namespace App\Services;
 
 use App\Models\Image;
 use App\Models\Tweet;
+use App\Modules\ImageUpload\ImageManagerInterface;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
 class TweetService
 {
+    public function __construct(private ImageManagerInterface $imageManager)
+    {
+    }
+
     public function getTweets()
     {
         return Tweet::with('images')->orderBy('created_at', 'DESC')->get();
@@ -40,9 +45,9 @@ class TweetService
             $tweet->content = $content;
             $tweet->save();
             foreach ($images as $image) {
-                Storage::putFile('public/images', $image);
+                $name = $this->imageManager->save($image);
                 $imageModel = new Image();
-                $imageModel->name = $image->hashName();
+                $imageModel->name = $name;
                 $imageModel->save();
                 $tweet->images()->attach($imageModel->id);
             }
@@ -54,14 +59,10 @@ class TweetService
         DB::transaction(function () use ($tweetId) {
             $tweet = Tweet::where('id', $tweetId)->firstOrFail();
             $tweet->images()->each(function ($image) use ($tweet) {
-                $filePath = 'public/images/' . $image->name;
-                if (Storage::exists($filePath)) {
-                    Storage::delete($filePath);
-                }
+                $this->imageManager->delete($image->name);
                 $tweet->images()->detach($image->id);
                 $image->delete();
             });
-
             $tweet->delete();
         });
     }
